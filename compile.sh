2380806220819ca1d187a78912ab0f26ea3e523e
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-11-16 22:13:07 +0100

    Updated compile.sh script

--- compile.sh
+++ compile.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 echo "Creating resource pack..."
-node install.js
+node generate.js
 
 echo "Moving LICENSE..."
 mv LICENSE pack/
-
-echo "Zipping..."
-zip -r resource-pack.zip pack/*
+cd pack
