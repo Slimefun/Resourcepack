cd86e6971821afaa01a6e4f9fdf0a4f7251cf94f
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-10-11 13:43:24 +0200

    Added namespaces

--- src/templates.js
+++ src/templates.js
@@ -3,62 +3,62 @@ const fs = require('fs').promises;
 module.exports = {
     ITEM: (item, overrides) => {
         fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "item/generated",
+            parent: "minecraft:item/generated",
             textures: {
-                "layer0": "item/" + item
+                "layer0": "minecraft:item/" + item
             },
             overrides: overrides
         }), "UTF-8");
     },
     FLAT_BLOCK: (item, overrides) => {
         fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "item/generated",
+            parent: "minecraft:item/generated",
             textures: {
-                "layer0": "block/" + item
+                "layer0": "minecraft:block/" + item
             },
             overrides: overrides
         }), "UTF-8");
     },
 	GLASS_PANE: (item, overrides) => {
         fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "item/generated",
+            parent: "minecraft:item/generated",
             textures: {
-                "layer0": "block/" + item.substring(0, item.length - 5)
+                "layer0": "minecraft:block/" + item.substring(0, item.length - 5)
             },
             overrides: overrides
         }), "UTF-8");
     },
 	FIREWORK_STAR: (item, overrides) => {
 		fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "item/generated",
+            parent: "minecraft:item/generated",
             textures: {
-                "layer0": "item/firework_star",
-				"layer1": "item/firework_star_overlay"
+                "layer0": "minecraft:item/firework_star",
+				"layer1": "minecraft:item/firework_star_overlay"
             },
             overrides: overrides
         }), "UTF-8");
 	},
 	SKULL: (item, overrides) => {
 		fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "item/template_skull",
+            parent: "minecraft:item/template_skull",
             overrides: overrides
         }), "UTF-8");
 	},
 	BLOCK: (item, overrides) => {
 		fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "block/" + item,
+            parent: "minecraft:block/" + item,
             overrides: overrides
         }), "UTF-8");
 	},
 	PISTON: (item, overrides) => {
 		fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "block/piston_inventory",
+            parent: "minecraft:block/piston_inventory",
             overrides: overrides
         }), "UTF-8");
 	},
 	HANDHELD: (item, overrides) => {
 		fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "item/handheld",
+            parent: "minecraft:item/handheld",
             textures: {
                 "layer0": "item/" + item
             },
@@ -67,10 +67,10 @@ module.exports = {
 	},
 	LEATHER_ARMOR: (item, overrides) => {
 		fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "item/generated",
+            parent: "minecraft:item/generated",
             textures: {
-                "layer0": "item/" + item,
-				"layer1": "item/" + item + "_overlay"
+                "layer0": "minecraft:item/" + item,
+				"layer1": "minecraft:item/" + item + "_overlay"
             },
             overrides: overrides
         }), "UTF-8");
@@ -81,400 +81,400 @@ module.exports = {
 			  "predicate": {
 				"time": 0
 			  },
-			  "model": "item/clock"
+			  "model": "minecraft:item/clock"
 			},
 			{
 			  "predicate": {
 				"time": 0.0078125
 			  },
-			  "model": "item/clock_01"
+			  "model": "minecraft:item/clock_01"
 			},
 			{
 			  "predicate": {
 				"time": 0.0234375
 			  },
-			  "model": "item/clock_02"
+			  "model": "minecraft:item/clock_02"
 			},
 			{
 			  "predicate": {
 				"time": 0.0390625
 			  },
-			  "model": "item/clock_03"
+			  "model": "minecraft:item/clock_03"
 			},
 			{
 			  "predicate": {
 				"time": 0.0546875
 			  },
-			  "model": "item/clock_04"
+			  "model": "minecraft:item/clock_04"
 			},
 			{
 			  "predicate": {
 				"time": 0.0703125
 			  },
-			  "model": "item/clock_05"
+			  "model": "minecraft:item/clock_05"
 			},
 			{
 			  "predicate": {
 				"time": 0.0859375
 			  },
-			  "model": "item/clock_06"
+			  "model": "minecraft:item/clock_06"
 			},
 			{
 			  "predicate": {
 				"time": 0.1015625
 			  },
-			  "model": "item/clock_07"
+			  "model": "minecraft:item/clock_07"
 			},
 			{
 			  "predicate": {
 				"time": 0.1171875
 			  },
-			  "model": "item/clock_08"
+			  "model": "minecraft:item/clock_08"
 			},
 			{
 			  "predicate": {
 				"time": 0.1328125
 			  },
-			  "model": "item/clock_09"
+			  "model": "minecraft:item/clock_09"
 			},
 			{
 			  "predicate": {
 				"time": 0.1484375
 			  },
-			  "model": "item/clock_10"
+			  "model": "minecraft:item/clock_10"
 			},
 			{
 			  "predicate": {
 				"time": 0.1640625
 			  },
-			  "model": "item/clock_11"
+			  "model": "minecraft:item/clock_11"
 			},
 			{
 			  "predicate": {
 				"time": 0.1796875
 			  },
-			  "model": "item/clock_12"
+			  "model": "minecraft:item/clock_12"
 			},
 			{
 			  "predicate": {
 				"time": 0.1953125
 			  },
-			  "model": "item/clock_13"
+			  "model": "minecraft:item/clock_13"
 			},
 			{
 			  "predicate": {
 				"time": 0.2109375
 			  },
-			  "model": "item/clock_14"
+			  "model": "minecraft:item/clock_14"
 			},
 			{
 			  "predicate": {
 				"time": 0.2265625
 			  },
-			  "model": "item/clock_15"
+			  "model": "minecraft:item/clock_15"
 			},
 			{
 			  "predicate": {
 				"time": 0.2421875
 			  },
-			  "model": "item/clock_16"
+			  "model": "minecraft:item/clock_16"
 			},
 			{
 			  "predicate": {
 				"time": 0.2578125
 			  },
-			  "model": "item/clock_17"
+			  "model": "minecraft:item/clock_17"
 			},
 			{
 			  "predicate": {
 				"time": 0.2734375
 			  },
-			  "model": "item/clock_18"
+			  "model": "minecraft:item/clock_18"
 			},
 			{
 			  "predicate": {
 				"time": 0.2890625
 			  },
-			  "model": "item/clock_19"
+			  "model": "minecraft:item/clock_19"
 			},
 			{
 			  "predicate": {
 				"time": 0.3046875
 			  },
-			  "model": "item/clock_20"
+			  "model": "minecraft:item/clock_20"
 			},
 			{
 			  "predicate": {
 				"time": 0.3203125
 			  },
-			  "model": "item/clock_21"
+			  "model": "minecraft:item/clock_21"
 			},
 			{
 			  "predicate": {
 				"time": 0.3359375
 			  },
-			  "model": "item/clock_22"
+			  "model": "minecraft:item/clock_22"
 			},
 			{
 			  "predicate": {
 				"time": 0.3515625
 			  },
-			  "model": "item/clock_23"
+			  "model": "minecraft:item/clock_23"
 			},
 			{
 			  "predicate": {
 				"time": 0.3671875
 			  },
-			  "model": "item/clock_24"
+			  "model": "minecraft:item/clock_24"
 			},
 			{
 			  "predicate": {
 				"time": 0.3828125
 			  },
-			  "model": "item/clock_25"
+			  "model": "minecraft:item/clock_25"
 			},
 			{
 			  "predicate": {
 				"time": 0.3984375
 			  },
-			  "model": "item/clock_26"
+			  "model": "minecraft:item/clock_26"
 			},
 			{
 			  "predicate": {
 				"time": 0.4140625
 			  },
-			  "model": "item/clock_27"
+			  "model": "minecraft:item/clock_27"
 			},
 			{
 			  "predicate": {
 				"time": 0.4296875
 			  },
-			  "model": "item/clock_28"
+			  "model": "minecraft:item/clock_28"
 			},
 			{
 			  "predicate": {
 				"time": 0.4453125
 			  },
-			  "model": "item/clock_29"
+			  "model": "minecraft:item/clock_29"
 			},
 			{
 			  "predicate": {
 				"time": 0.4609375
 			  },
-			  "model": "item/clock_30"
+			  "model": "minecraft:item/clock_30"
 			},
 			{
 			  "predicate": {
 				"time": 0.4765625
 			  },
-			  "model": "item/clock_31"
+			  "model": "minecraft:item/clock_31"
 			},
 			{
 			  "predicate": {
 				"time": 0.4921875
 			  },
-			  "model": "item/clock_32"
+			  "model": "minecraft:item/clock_32"
 			},
 			{
 			  "predicate": {
 				"time": 0.5078125
 			  },
-			  "model": "item/clock_33"
+			  "model": "minecraft:item/clock_33"
 			},
 			{
 			  "predicate": {
 				"time": 0.5234375
 			  },
-			  "model": "item/clock_34"
+			  "model": "minecraft:item/clock_34"
 			},
 			{
 			  "predicate": {
 				"time": 0.5390625
 			  },
-			  "model": "item/clock_35"
+			  "model": "minecraft:item/clock_35"
 			},
 			{
 			  "predicate": {
 				"time": 0.5546875
 			  },
-			  "model": "item/clock_36"
+			  "model": "minecraft:item/clock_36"
 			},
 			{
 			  "predicate": {
 				"time": 0.5703125
 			  },
-			  "model": "item/clock_37"
+			  "model": "minecraft:item/clock_37"
 			},
 			{
 			  "predicate": {
 				"time": 0.5859375
 			  },
-			  "model": "item/clock_38"
+			  "model": "minecraft:item/clock_38"
 			},
 			{
 			  "predicate": {
 				"time": 0.6015625
 			  },
-			  "model": "item/clock_39"
+			  "model": "minecraft:item/clock_39"
 			},
 			{
 			  "predicate": {
 				"time": 0.6171875
 			  },
-			  "model": "item/clock_40"
+			  "model": "minecraft:item/clock_40"
 			},
 			{
 			  "predicate": {
 				"time": 0.6328125
 			  },
-			  "model": "item/clock_41"
+			  "model": "minecraft:item/clock_41"
 			},
 			{
 			  "predicate": {
 				"time": 0.6484375
 			  },
-			  "model": "item/clock_42"
+			  "model": "minecraft:item/clock_42"
 			},
 			{
 			  "predicate": {
 				"time": 0.6640625
 			  },
-			  "model": "item/clock_43"
+			  "model": "minecraft:item/clock_43"
 			},
 			{
 			  "predicate": {
 				"time": 0.6796875
 			  },
-			  "model": "item/clock_44"
+			  "model": "minecraft:item/clock_44"
 			},
 			{
 			  "predicate": {
 				"time": 0.6953125
 			  },
-			  "model": "item/clock_45"
+			  "model": "minecraft:item/clock_45"
 			},
 			{
 			  "predicate": {
 				"time": 0.7109375
 			  },
-			  "model": "item/clock_46"
+			  "model": "minecraft:item/clock_46"
 			},
 			{
 			  "predicate": {
 				"time": 0.7265625
 			  },
-			  "model": "item/clock_47"
+			  "model": "minecraft:item/clock_47"
 			},
 			{
 			  "predicate": {
 				"time": 0.7421875
 			  },
-			  "model": "item/clock_48"
+			  "model": "minecraft:item/clock_48"
 			},
 			{
 			  "predicate": {
 				"time": 0.7578125
 			  },
-			  "model": "item/clock_49"
+			  "model": "minecraft:item/clock_49"
 			},
 			{
 			  "predicate": {
 				"time": 0.7734375
 			  },
-			  "model": "item/clock_50"
+			  "model": "minecraft:item/clock_50"
 			},
 			{
 			  "predicate": {
 				"time": 0.7890625
 			  },
-			  "model": "item/clock_51"
+			  "model": "minecraft:item/clock_51"
 			},
 			{
 			  "predicate": {
 				"time": 0.8046875
 			  },
-			  "model": "item/clock_52"
+			  "model": "minecraft:item/clock_52"
 			},
 			{
 			  "predicate": {
 				"time": 0.8203125
 			  },
-			  "model": "item/clock_53"
+			  "model": "minecraft:item/clock_53"
 			},
 			{
 			  "predicate": {
 				"time": 0.8359375
 			  },
-			  "model": "item/clock_54"
+			  "model": "minecraft:item/clock_54"
 			},
 			{
 			  "predicate": {
 				"time": 0.8515625
 			  },
-			  "model": "item/clock_55"
+			  "model": "minecraft:item/clock_55"
 			},
 			{
 			  "predicate": {
 				"time": 0.8671875
 			  },
-			  "model": "item/clock_56"
+			  "model": "minecraft:item/clock_56"
 			},
 			{
 			  "predicate": {
 				"time": 0.8828125
 			  },
-			  "model": "item/clock_57"
+			  "model": "minecraft:item/clock_57"
 			},
 			{
 			  "predicate": {
 				"time": 0.8984375
 			  },
-			  "model": "item/clock_58"
+			  "model": "minecraft:item/clock_58"
 			},
 			{
 			  "predicate": {
 				"time": 0.9140625
 			  },
-			  "model": "item/clock_59"
+			  "model": "minecraft:item/clock_59"
 			},
 			{
 			  "predicate": {
 				"time": 0.9296875
 			  },
-			  "model": "item/clock_60"
+			  "model": "minecraft:item/clock_60"
 			},
 			{
 			  "predicate": {
 				"time": 0.9453125
 			  },
-			  "model": "item/clock_61"
+			  "model": "minecraft:item/clock_61"
 			},
 			{
 			  "predicate": {
 				"time": 0.9609375
 			  },
-			  "model": "item/clock_62"
+			  "model": "minecraft:item/clock_62"
 			},
 			{
 			  "predicate": {
 				"time": 0.9765625
 			  },
-			  "model": "item/clock_63"
+			  "model": "minecraft:item/clock_63"
 			},
 			{
 			  "predicate": {
 				"time": 0.9921875
 			  },
-			  "model": "item/clock"
+			  "model": "minecraft:item/clock"
 			}
 		]
 		
 		overrides = faces.concat(overrides);
 		
 		fs.writeFile(`assets/minecraft/models/item/${item}.json`, JSON.stringify({
-            parent: "item/generated",
+            parent: "minecraft:item/generated",
             textures: {
-                "layer0": "item/clock_00",
+                "layer0": "minecraft:item/clock_00",
             },
             overrides: overrides
         }), "UTF-8");
